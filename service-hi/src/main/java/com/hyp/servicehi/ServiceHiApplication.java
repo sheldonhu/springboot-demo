6e4537bcc0a2f7346eef5dcf20782244fc75901b
huyupeng <huyp1024@163.com>
2018-10-01 11:33:37 +0800

    initial git repository

---
+++ service-hi/src/main/java/com/hyp/servicehi/ServiceHiApplication.java
@@ -0,0 +1,18 @@
+package com.hyp.servicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+}
